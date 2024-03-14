eab37566bba79de2054611e2a0d84150555fdd59
Oltion-Koceku <koceku17@gmail.com>
2024-03-14 12:32:56 +0100

    [add]Milestone 1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+import { contacts } from "./contacts.js";
+
+const { createApp } = Vue;
+
+createApp({
+  data(){
+    return{
+      contacts,
+
+    }
+  },
+
+  methods:{
+    
+  },
+
+  mounted(){
+    
+  }
+}).mount("#app")
