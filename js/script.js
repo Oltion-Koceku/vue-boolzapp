e835124d1d490bd212547315156b23ad39554e2b
Oltion-Koceku <koceku17@gmail.com>
2024-03-15 12:36:52 +0100

    [add]milstone 5 da fixxare

--- js/script.js
+++ js/script.js
@@ -1,44 +1,50 @@
+const { DateTime } = luxon;
+
 import { contacts } from "./contacts.js";
 
 const { createApp } = Vue;
 
 createApp({
-  data(){
-    return{
+  data() {
+    return {
       contacts,
       // messaggio che mando
-      newMyText:{
-        date: '10/01/2020 15:50:00',
+      newMyText: {
+        date: '',
         message: '',
         status: 'sent'
       },
       // messaggio che ricevo
-      newUserText : {
-        date: '10/01/2020 15:50:00',
+      newUserText: {
+        date: '',
         message: 'OK',
         status: 'received'
       },
+      // ricerca
+      search: '',
 
-      search : ''
+      chevronClick : false
+
     }
   },
 
-  methods:{
-    changeUser(indice){
+
+  methods: {
+    changeUser(indice) {
       // reset per il cambio chat
       this.contacts.forEach(contact => {
         contact.visible = true
-      }); 
+      });
       // rendo visibile la chat
       this.contacts[indice].visible = false
     },
 
-    enterMyMessage(indice){
+    enterMyMessage(indice) {
       if (this.newMyText.message.length + 1 > 0) {
         this.contacts[indice].messages.push(this.newMyText)
         // resetto newMyText
-        this.newMyText={
-          date: '10/01/2020 15:50:00',
+        this.newMyText = {
+          date: '',
           message: '',
           status: 'sent'
         }
@@ -46,29 +52,56 @@ createApp({
 
     },
 
-    userMessage(indice){
+    userMessage(indice) {
       // ricevo in ritardo il messaggio
-      setTimeout(()=>{
+      setTimeout(() => {
         this.contacts[indice].messages.push(this.newUserText)
-          console.log(this.newUserText.message);
+        console.log(this.newUserText.message);
       }, 1000)
     },
 
-    deleteMessage(indice, i){
+    deleteMessage(indice, i) {
       this.contacts[indice].messages.splice(i)
-    }
+    },
+
+    printMyData(){
+      this.newMyText.date = DateTime.now().setLocale('it').toFormat('dd/MM/yyyy hh:mm:ss')
+    },
     
-  },
-  
-  computed:{
-    searchContact(){
-      return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(this.search.toLocaleLowerCase()))
-    }
+    printUserData(){
+      setTimeout(() => {
+        this.newUserText.date = DateTime.now().setLocale('it').toFormat('dd/MM/yyyy hh:mm:ss')
+      }, 1000)
+    },
     
+    getLastMessage(contact){
+      if (contact.messages.length > 0) {
+        return contact.messages[contact.messages.length - 1].message
+      }
+    },
+
+    getLastDate(contact){
+      if (contact.messages.length > 0) {
+        return contact.messages[contact.messages.length - 1].date
+      }
+    },
+
+    resetChevron(){
+      chevronClick = false
+    }
+
   },
 
-  mounted(){
-    
+  computed: {
+    searchContact() {
+      return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(this.search.toLocaleLowerCase()))
+    }
+
+  },
+
+  mounted() {
+    this.printMyData()
+    this. printUserData()
   }
 }).mount("#app")
 
